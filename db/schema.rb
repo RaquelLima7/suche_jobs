cdf14ed28bd41d37a75ac69026d85fdae4de1ab0
RaquelLima7 <raquellimadev@gmail.com>
2021-10-10 16:30:21 -0300

    crud applicants, companies and vacancies

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_10_191556) do
+ActiveRecord::Schema.define(version: 2021_10_10_192938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "applicants", force: :cascade do |t|
+    t.string "name"
+    t.bigint "vacancy_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["vacancy_id"], name: "index_applicants_on_vacancy_id"
+  end
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +42,19 @@ ActiveRecord::Schema.define(version: 2021_10_10_191556) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "vacancies", force: :cascade do |t|
+    t.string "title"
+    t.string "location"
+    t.text "description"
+    t.string "requirements"
+    t.string "salary"
+    t.boolean "available"
+    t.bigint "company_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["company_id"], name: "index_vacancies_on_company_id"
+  end
+
+  add_foreign_key "applicants", "vacancies"
+  add_foreign_key "vacancies", "companies"
 end
