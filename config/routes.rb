8fa017ab8aa51d53c00e23b4acd876cdcf864303
RaquelLima7 <raquellimadev@gmail.com>
2022-01-31 16:48:33 -0300

    Added authenticate_company and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :companies
+  get '/vacancies' => 'vacancies#index', as: :company_root
   resources :applicants
   resources :vacancies
   resources :companies
   devise_for :users
-  root to: 'pages#home'
+  # root to: 'pages#vacancies'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
