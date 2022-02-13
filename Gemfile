71b37077d9d1682aa0b125b4c4f551e3a09c1d69
RaquelLima7 <raquellimadev@gmail.com>
2022-02-13 14:15:16 -0300

    Edit devise and added email to applicants

--- Gemfile
+++ Gemfile
@@ -31,7 +31,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 gem 'devise'
 
 gem 'autoprefixer-rails', '10.2.5'
-gem 'font-awesome-sass'
+gem 'font-awesome-sass', '~> 5.15.1'
 gem 'simple_form'
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_04_210920) do
+ActiveRecord::Schema.define(version: 2022_02_13_171426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2022_02_04_210920) do
     t.bigint "vacancy_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
     t.index ["vacancy_id"], name: "index_applicants_on_vacancy_id"
   end
 
